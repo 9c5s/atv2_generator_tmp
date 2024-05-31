00fdf4c50a1ccef5a8baddbb99aa6f2c827ca532
9c5s <346@ymail.ne.jp>
2024-05-31 13:33:07 +0900

    init

---
+++ ASOBI TICKET_files/inject.js
@@ -0,0 +1,40 @@
+function updateTime() {
+  const now = new Date();
+  const pad = (num) => ("0" + num).slice(-2);
+  document.querySelector(".year").textContent = now.getFullYear();
+  document.querySelector(".month").textContent = pad(now.getMonth() + 1);
+  document.querySelector(".day").textContent = pad(now.getDate());
+  document.querySelector(".hour.time-item").textContent = pad(now.getHours());
+  document.querySelector(".minute.time-item").textContent = pad(now.getMinutes());
+  document.querySelector(".second.time-item").textContent = pad(now.getSeconds());
+}
+
+function formatDate(dateString) {
+  const [year, month, day] = dateString.split("-");
+  const formattedMonth = month.replace(/^0+/, "");
+  const formattedDay = day.replace(/^0+/, "");
+  return `${year}年${formattedMonth}月${formattedDay}日`;
+}
+
+window.addEventListener("load", () => {
+  updateTime();
+  setInterval(updateTime, 1000);
+});
+
+window.onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const lastname = urlParams.get("lastname");
+  const firstname = urlParams.get("firstname");
+  const birthdate = urlParams.get("birthdate");
+  const managerNumber = urlParams.get("managerNumber");
+  const qrCode = localStorage.getItem("qrCode");
+
+  document.getElementById("fullName").textContent = `${lastname} ${firstname}`;
+  document.getElementById("birthDate").textContent = formatDate(birthdate);
+  document.getElementById("managerNumber").textContent = managerNumber;
+  if (qrCode) {
+    document.getElementById("qrCode").src = qrCode;
+  }
+
+  document.getElementById("ticket").style.display = "block";
+};
