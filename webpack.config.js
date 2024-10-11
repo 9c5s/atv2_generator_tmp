2feae286acfbf7d7ccd4603fe81338a5cf7306e5
9c5s <346@ymail.ne.jp>
2024-10-11 19:50:59 +0900

    記述ミス修正3

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,91 @@
 const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-const WebpackObfuscator = require("webpack-obfuscator"); // 正しいプラグインをインポート
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index/index.js", // メインエントリーポイント
+  entry: {
+    index: ["./src/index/index.js", "./src/index/index.css"],
+    form: ["./src/form/form.js", "./src/form/form.css"],
+    ASOBI_TICKET: [
+      "./src/ASOBI_TICKET/inject.js",
+      "./src/ASOBI_TICKET/otBannerSdk.js",
+      "./src/ASOBI_TICKET/otSDKStub.js",
+      "./src/ASOBI_TICKET/polyfills-ROJ3GJHA.js",
+      "./src/ASOBI_TICKET/script.js",
+      "./src/ASOBI_TICKET/uwt.js",
+      "./src/ASOBI_TICKET/styles.css",
+      "./src/ASOBI_TICKET/styles-EHIDZJXE.css",
+    ],
+  },
   output: {
-    filename: "js/bundle.js",
+    filename: "scripts/[name]_[contenthash].js",
     path: path.resolve(__dirname, "dist"),
-    clean: true, // ビルド前にdistディレクトリをクリーン
-    publicPath: "", // 必要に応じて設定
+    assetModuleFilename: "assets/[hash][ext][query]",
   },
   module: {
     rules: [
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader", // 必要に応じてBabelを使用
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
-        test: /\.(svg|png|jpg|jpeg|gif)$/, // 画像ファイルの処理
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-        generator: {
-          filename: "images/[name][ext]", // dist/images に出力
-        },
       },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: true,
-          mangle: true,
-        },
-      }),
-      new CssMinimizerPlugin(),
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
-    // HTML ファイルを処理するプラグイン
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "styles/[name]_[contenthash].css",
+    }),
     new HtmlWebpackPlugin({
-      template: "./src/index/index.html",
       filename: "index.html",
-      chunks: ["main"],
+      template: "./src/index.html",
+      chunks: ["index"],
     }),
     new HtmlWebpackPlugin({
-      template: "./src/form.html",
       filename: "form.html",
-      chunks: ["main"],
+      template: "./src/form.html",
+      chunks: ["form"],
     }),
     new HtmlWebpackPlugin({
-      template: "./src/ASOBI_TICKET.html",
       filename: "ASOBI_TICKET.html",
-      chunks: ["main"],
+      template: "./src/ASOBI_TICKET.html",
+      chunks: ["ASOBI_TICKET"],
     }),
-    new WebpackObfuscator(
-      {
-        rotateStringArray: true,
-        stringArray: true,
-        stringArrayEncoding: ["base64"],
-        stringArrayThreshold: 1,
-        // その他の難読化オプションをここに追加
-      },
-      ["js/bundle.js"]
-    ), // 対象ファイルを指定
   ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  mode: "production",
 };
